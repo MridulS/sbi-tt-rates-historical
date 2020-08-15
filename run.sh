1bc7a1a1bd648caa59285c8ff9a568c41e3235d8
Shivam Khandelwal (auto commit) <noreply@shivamkhandelwal.in>
2020-08-15 23:30:05 +0000

    change bot name/email

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 wget https://www.sbi.co.in/documents/16012/1400784/FOREX_CARD_RATES.pdf -O ~/sbi-tt-rates-historical/forex_sbi_tt_rates/$(date '+%Y-%m-%d-%H:%M').pdf
 git add .
-git config user.email "noreply@shivamkhandelwal.in"
-git config user.name "Shivam Khandelwal (auto commit)"
+git config user.email "sbi.tt.bot"
+git config user.name "SBI TT FOREX BOT(auto commit)"
 git commit -m "Daily update $(date '+%Y-%m-%d-%H:%M')"
 git push origin master
